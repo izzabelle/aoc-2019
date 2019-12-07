fcf672b3f3b2a8f181a38243cba607ea761911ff
Isabelle L <me@izzabelle.dev>
2019-12-07 04:36:20 -0600

    staging for day six

--- src/main.rs
+++ src/main.rs
@@ -60,6 +60,14 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             }
         }
 
+        6 => {
+            if !opt.part {
+                days::day_six::part_one();
+            } else {
+                days::day_six::part_two();
+            }
+        }
+
         _ => {
             println!("day {} is either not valid or incomplete", day);
         }
